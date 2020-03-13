e5fbd76aad627baa4f38956b7ce785ad2e1e988d
Bartosz Ujazdowski <b.ujazdowski@gmail.com>
2020-03-13 13:49:53 +0100

    Login form

--- app/controllers/LoginController.scala
+++ app/controllers/LoginController.scala
@@ -11,11 +11,10 @@ import play.api.mvc.{Action, AnyContent, MessagesAbstractController, MessagesCon
 class LoginController @Inject()(userDAO: UserDAO,
                                 cc: MessagesControllerComponents) extends MessagesAbstractController(cc) {
 
-  private val email = "Email"
   private val password = "Password"
 
   val loginForm: Form[LoginForm] = Form(mapping(
-    email -> text(3, 255),
+    "Email" -> email,
     password -> text(8)
   )(LoginForm.apply)(LoginForm.unapply))
 
@@ -24,23 +23,26 @@ class LoginController @Inject()(userDAO: UserDAO,
   }
 
   def validateLogin: Action[AnyContent] = Action { implicit request =>
-    val postVals = request.body.asFormUrlEncoded
-    postVals.map { args =>
-      val emailValue = args.getOrElse(email, Seq.empty).headOption.getOrElse("")
-      val passwordValue = args.getOrElse(password, Seq.empty).headOption.getOrElse("")
-      userDAO.findByEmail(emailValue) match {
-        case Some(user) =>
-          if (user.checkPassword(passwordValue)) {
-            Redirect(routes.HomeController.index()).withSession(
-              "email" -> user.email,
-              "id" -> user.id.toString
-            )
-          } else {
-            Redirect(routes.LoginController.login()).flashing("error" -> "Invalid email or password")
-          }
-        case None => Redirect(routes.LoginController.login()).flashing("error" -> "User with given email does not exists")
+
+    loginForm.bindFromRequest.fold(
+      formWithErrors => {
+        BadRequest(views.html.login(formWithErrors))
+      },
+      loginData => {
+        userDAO.findByEmail(loginData.email) match {
+          case Some(user) =>
+            if (user.checkPassword(loginData.password)) {
+              Redirect(routes.HomeController.index()).withSession(
+                "email" -> user.email,
+                "id" -> user.id.toString
+              )
+            } else {
+              Redirect(routes.LoginController.login()).flashing("error" -> "Invalid email or password")
+            }
+          case None => Redirect(routes.LoginController.login()).flashing("error" -> "User with given email does not exists")
+        }
       }
-    }.getOrElse(Redirect(routes.LoginController.login()))
+    )
   }
 
   def logout: Action[AnyContent] = Action {
