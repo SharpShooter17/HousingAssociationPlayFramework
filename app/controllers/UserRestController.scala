0c5318fadf05afebce35ec373ec0a417c7e784b4
Bartosz Ujazdowski <b.ujazdowski@gmail.com>
2020-04-25 17:28:59 +0200

    Rest - Create user

--- app/controllers/UserRestController.scala
+++ app/controllers/UserRestController.scala
@@ -1,11 +1,15 @@
 package controllers
 
 import dao.UserDAO
+import exceptions.AppException
 import javax.inject.{Inject, Singleton}
 import model.domain.User
+import model.form.UserForm
 import play.api.i18n.I18nSupport
-import play.api.libs.json.{JsValue, Json, Writes}
-import play.api.mvc.{AbstractController, ControllerComponents, EssentialAction, Result}
+import play.api.libs.functional.syntax._
+import play.api.libs.json.Reads._
+import play.api.libs.json._
+import play.api.mvc.{AbstractController, ControllerComponents}
 import services.HousingAssociationService
 
 import scala.concurrent.ExecutionContext
@@ -14,7 +18,7 @@ import scala.concurrent.ExecutionContext
 class UserRestController @Inject()(cc: ControllerComponents, service: HousingAssociationService)
                                   (implicit userDAO: UserDAO, executionContext: ExecutionContext) extends AbstractController(cc) with I18nSupport {
 
-  implicit def userWrites = new Writes[User] {
+  implicit val userWrites = new Writes[User] {
     override def writes(o: User): JsValue = Json.obj(
       "email" -> o.email,
       "name" -> o.firstName,
@@ -25,8 +29,25 @@ class UserRestController @Inject()(cc: ControllerComponents, service: HousingAss
     )
   }
 
+  implicit val userFormReads: Reads[UserForm] = (
+    (JsPath \ "firstName").read[String] and
+      (JsPath \ "lastName").read[String] and
+      (JsPath \ "telephone").read[String] and
+      (JsPath \ "email").read[String] and
+      (JsPath \ "roles").read[Set[String]]
+    ) (UserForm.apply _)
+
   def users() = Action {
     Ok(Json.arr(userDAO.all().map(u => Json.toJson(u))))
+  }
+
+  def addUser() = Action { implicit request =>
+    val json = request.body.asJson.get
+    json.validate[UserForm] match {
+      case s: JsSuccess[UserForm] => service.addUser(s.get)
+      case _: JsError => throw AppException()
+    }
+    Created("User created")
   }
 
 }
